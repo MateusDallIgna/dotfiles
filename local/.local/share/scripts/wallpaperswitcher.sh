efe511d6ffd9e5b40c8c47c79573974dcca06c0b
MateusDalligna <mateusdalligna.ifc@gmail.com>
2026-01-02 18:36:46 -0300

    Add a style to wallpaperswitcher

--- local/.local/share/scripts/wallpaperswitcher.sh
+++ local/.local/share/scripts/wallpaperswitcher.sh
@@ -8,7 +8,7 @@ WALLPAPER_PATH=$HOME/.local/share/themes/$CURRENT_THEME/wallpapers
 
 cd $WALLPAPER_PATH || exit
 
-SELECTED_WALLPAPER=$(for theme in *.jpg *.png *.gif; do echo -en "$theme\0icon\x1f$theme\n" ; done | rofi -dmenu -p "")  
+SELECTED_WALLPAPER=$(for theme in *.jpg *.png *.gif; do echo -en "$theme\0icon\x1f$theme\n" ; done | rofi -dmenu -p "" -theme ~/.config/rofi/styles/wallpaperluncher.rasi)
     
 if [ -n "$SELECTED_WALLPAPER" ]; then
     swww img -t center --transition-duration 3 --transition-fps 60 "$WALLPAPER_PATH/$SELECTED_WALLPAPER"
