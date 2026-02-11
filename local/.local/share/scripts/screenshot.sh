319b8c32fc34e8c38fca84191fb1507bb78f346e
MateusDalligna <mateusdalligna.ifc@gmail.com>
2026-02-10 21:33:03 -0300

    add wlogout

--- local/.local/share/scripts/screenshot.sh
+++ local/.local/share/scripts/screenshot.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-hyprshot -m region --raw | satty -f - --copy-command wl-copy
+MODE=$1
+
+case $MODE in
+
+    region)
+        hyprshot -m region --raw | satty -f - --copy-command wl-copy
+        ;;
+    window)
+        hyprshot -m window --raw | satty -f - --copy-command wl-copy
+        ;;
+esac
