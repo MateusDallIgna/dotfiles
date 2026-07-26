56020e94931bc6dd9ce9041dbfb057390c1d53da
MateusDalligna <mateusdalligna.ifc@gmail.com>
2026-07-26 17:00:23 -0300

    feat: new waybar style Sharp

--- local/.local/share/scripts/wallpaperswitcher.sh
+++ local/.local/share/scripts/wallpaperswitcher.sh
@@ -3,19 +3,25 @@
 CWD=$(pwd)
 
 CURRENT_THEME="$(head -n 1 ~/.cache/current_theme.txt)"
-
 WALLPAPER_PATH=$HOME/.local/share/themes/$CURRENT_THEME/wallpapers
 
-cd $WALLPAPER_PATH || exit
+cd "$WALLPAPER_PATH" || exit
 
-SELECTED_WALLPAPER=$(for theme in *.jpg *.png *.gif; do echo -en "$theme\0icon\x1f$theme\n" ; done | rofi -i -dmenu -p "" -theme ~/.config/rofi/styles/wallpaperluncher.rasi)
-    
+CACHE_DIR="/tmp/rofi-wp-cache/$CURRENT_THEME"
+mkdir -p "$CACHE_DIR"
+
+SELECTED_WALLPAPER=$(for theme in *.jpg *.png *.gif; do
+    thumb="$CACHE_DIR/${theme%.*}.png"
+    if [ "$theme" -nt "$thumb" ]; then
+        convert "$theme" -resize x500 -gravity center -crop 340x500+0+0 +repage "$thumb"
+    fi
+    echo -en "$theme\0icon\x1f$thumb\n"
+done | rofi -i -dmenu -p "" -theme ~/.config/rofi/styles/wallpaperluncher.rasi)
+
 if [ -n "$SELECTED_WALLPAPER" ]; then
     awww img -t center --transition-duration 3 --transition-fps 60 "$WALLPAPER_PATH/$SELECTED_WALLPAPER"
-    echo "$SELECTED_WALLPAPER" > $HOME/.local/share/themes/$CURRENT_THEME/wall.set
-    ## set rofi background image
-    cp "$WALLPAPER_PATH/$SELECTED_WALLPAPER" $HOME/.config/rofi/backgrounds/current_wallpaper.png
-
+    echo "$SELECTED_WALLPAPER" > "$HOME/.local/share/themes/$CURRENT_THEME/wall.set"
+    cp "$WALLPAPER_PATH/$SELECTED_WALLPAPER" "$HOME/.config/rofi/backgrounds/current_wallpaper.png"
 fi
 
-cd $CWD || exit
+cd "$CWD" || exit
